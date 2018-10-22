9af9ba7550095415939a61ed5226e0a1363d6aef
Joakim Rosenqvist <jr222wb@student.lnu.se>
2018-10-22 16:25:25 +0200

    Gubbfläsk

--- BlackJack/Program.java
+++ BlackJack/Program.java
@@ -10,8 +10,8 @@ public class Program {
   public static void main(String[] a_args) {
 
     Game g = new Game(new RulesCrazy()); // RulesStandard, RulesCrazy and RulesGoofy available
-    //IView v = new SimpleView(); // new SwedishView();
-    IView v = new SwedishView();
+    IView v = new SimpleView(); // new SwedishView();
+    // IView v = new SwedishView();
     PlayGame ctrl = new PlayGame(g, v);
 
     while (ctrl.Play())
